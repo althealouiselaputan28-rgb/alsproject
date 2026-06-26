334d50c6b1b5025a791d161f8fbddf98ca4b285e
althealouiselaputan28-rgb <althealouiselaputan28@gmail.com>
2026-06-26 16:09:26 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   root: '.',
   build: {
     outDir: 'dist',
     rollupOptions: {
-      input: 'index.html'
+      input: {
+        index: resolve(__dirname, 'index.html'),
+        main: resolve(__dirname, 'main.html')
+      }
     }
   }
 });
