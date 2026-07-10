f28873889322562cb5977d0f586bcc3ac6ad834b
althealouiselaputan28-rgb <althealouiselaputan28@gmail.com>
2026-07-10 14:32:56 +0800

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,10 @@ export default defineConfig({
     rollupOptions: {
       input: {
         index: resolve(__dirname, 'index.html'),
-        main: resolve(__dirname, 'main.html')
+        main: resolve(__dirname, 'main.html'),
+        eventCalendar: resolve(__dirname, 'event-calendar.html'),
+        resources: resolve(__dirname, 'resources.html'),
+        collegeAdmissionHub: resolve(__dirname, 'college-admission-hub.html')
       }
     }
   }
